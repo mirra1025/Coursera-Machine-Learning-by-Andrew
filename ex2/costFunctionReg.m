8e8283ee5be2bf7b8ee64b0067d811b035cf3896
Mengyuan <mirra1025@gmail.com>
2020-11-12 14:20:36 +0800

    Update costFunctionReg.m

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,10 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+  J = 1/m * sum( -y.*log(sigmoid(X*theta)) - (1-y).*log(1-sigmoid(X*theta)) );
+  theta(1,1) = 0; 
+  % Note that you should not regularize the parameter theta0, so set theta0 to be 0 
+  J = J + 1/(2*m)*lambda*sum(theta.^2);
 
-
-
-
+  grad = 1/m * ( X'*(sigmoid(X*theta)-y) ) + 1/m * lambda*theta;
 
 % =============================================================
 
